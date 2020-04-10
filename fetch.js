c56006ac3c6c9ec3f3ea48e8621618f928789267
Soumik Acharjee <acharjee266@gmail.com>
2020-04-10 22:41:02 +0530

    tweaked ui

--- fetch.js
+++ fetch.js
@@ -14,20 +14,21 @@ function loadPage() {
   }
   div.innerHTML = `
   <div class="row container">
-    <div class="col-md-5 pt-4"><img src=${cur_movie.img} />
+    <div class="col-md-6 pt-4"><img src=${cur_movie.img} />
     <h2>${cur_movie.name}</h2>
     </div>
-<div class="col-md-5">
+<div class="col-md-6">
 <div>
 <h2>seats</h2>
-<div class="row col-md-8 border rounded border-dark shadow-lg p-4">
+<div class="row col-md-12 border rounded border-dark shadow-lg p-4">
 ${cur_movie.seats.map((ele, idx) => {
   if (ele) {
     return `<div><img
         class="icon is_active"
         src="https://image.flaticon.com/icons/svg/2052/2052398.svg"
       />
-      <p>${idx + 1}</p>
+      <p class="text-dark text-center">${idx + 1}</p>
+      <hr />
       </div>`;
   } else {
     return `<div><img
@@ -53,7 +54,7 @@ window.addEventListener("DOMContentLoaded", loadPage);
 let tiks = [];
 function select(idx) {
   let div = document.createElement("div");
-  div.className = "";
+  div.className = "col-md-12";
   if (tiks[idx] == true) {
     event.target.style.backgroundColor = "";
     price -= cur_movie.price;
@@ -63,10 +64,12 @@ function select(idx) {
     price += cur_movie.price;
     event.target.style.backgroundColor = "green";
   }
-  div.innerHTML = `<div class="bg-dark">
-                    <h2 class="" >your total cost is ${price}</h2>
-                    <button class="" onclick ="payment()">Payment</button>
-                    </div>`;
+  div.innerHTML = `<div class="border rounded p-4 shadow-lg mt-4 ">
+                    <h2>no of seats: </h2>
+                    <p class="lead" >your total cost is Rs ${price}</p>
+                    <button class="btn btn-outline-success" onclick ="payment()">Pay Now</button>
+                    </div>
+                    `;
   paySec.innerHTML = "";
   paySec.appendChild(div);
 }
